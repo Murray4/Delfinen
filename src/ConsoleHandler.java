295f1cb8320b80bad3926973bcd12b250b8b596e
Murray4 <sebastianmurraysimonsen@gmail.com>
2025-05-07 09:13:58 +0200

    Eksempel på menu (memberMenu)

--- src/ConsoleHandler.java
+++ src/ConsoleHandler.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class ConsoleHandler {
 
     public static void main(String[] args) {}
-
+    Scanner scanner = new Scanner(System.in);
 
     // METODER (- main)
     public static void run() {}
@@ -12,6 +14,27 @@ public class ConsoleHandler {
 
     public static void trainerMenu() {}
 
-    public static void memberMenu() {}
+
+    public static void memberMenu(Scanner scanner) {
+        System.out.println(memberMenuTekst());
+
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+    public static String memberMenuTekst() {
+        return """
+                1. Register Member
+                2. Edit Member
+                3. Search Member
+                """;
+    }
 
 }
