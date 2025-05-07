2713f8bed33fd4524b080306eec5d7f0a67d902f
Rayyadoshe <rado0001@stud.kea.dk>
2025-05-07 10:03:36 +0200

    Raya commit 1

--- src/Member.java
+++ src/Member.java
@@ -18,7 +18,42 @@ public class Member {
     int memberID;
 
 
-    // METODER
-    public String toString() {}
+    public Member(String memberName, Membership membership, boolean isActive, String email,
+                  int phoneNumber, Boolean isCompetitionSwimmer, boolean hasPayed, boolean isSenior,
+                  Map<Dicipline, TrainingResult> trainingResult, ArrayList<CompetitionResult> competitionResult,
+                  int memberPrice, int memberID) {
+        this.memberName = memberName;
+        this.membership = membership;
+        this.isActive = isActive;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.isCompetitionSwimmer = isCompetitionSwimmer;
+        this.hasPayed = hasPayed;
+        this.isSenior = isSenior;
+        this.trainingResult = trainingResult;
+        this.competitionResult = competitionResult;
+        this.memberPrice = memberPrice;
+        this.memberID = memberID;
+    }
 
-}
+
+    // METODER
+    @Override
+    public String toString() {
+            return "Member{" +
+                    "memberName='" + memberName + '\'' +
+                    ", membership=" + membership +
+                    ", isActive=" + isActive +
+                    ", email='" + email + '\'' +
+                    ", phoneNumber=" + phoneNumber +
+                    ", isCompetitionSwimmer=" + isCompetitionSwimmer +
+                    ", hasPayed=" + hasPayed +
+                    ", isSenior=" + isSenior +
+                    ", trainingResult=" + trainingResult +
+                    ", competitionResult=" + competitionResult +
+                    ", memberPrice=" + memberPrice +
+                    ", memberID=" + memberID +
+                    '}';
+        }
+    }
+
