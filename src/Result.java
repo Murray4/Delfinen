fe33114f62b9fda59f5a880d2ad83ed4443e53c8
MagnusBD <magnusdam527@outlook.dk>
2025-05-13 04:56:41 +0200

    Main

--- src/Result.java
+++ src/Result.java
@@ -1,11 +1,10 @@
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Result {
 
     // ATTRIBUTTER
-    Dicipline dicipline;
+    private Dicipline dicipline;
     LocalTime time;             // muligvis bare int
     LocalDate date;
     Member member;
@@ -13,5 +12,13 @@ public class Result {
     // METODER
     public String toString() {
         return ", Tid: " + time + ", Dato: " + date;
+    }
+
+    public Dicipline getDicipline() {
+        return dicipline;
+    }
+
+    public void setDicipline(Dicipline dicipline) {
+        this.dicipline = dicipline;
     }
 }
